ae8c8403e349d3b2bf2048aca65df720d7cb90f0
Adriano Guterres <guterres.adriano@gmail.com>
2023-07-11 23:33:07 -0300

    First project commit

---
+++ src/test/java/com/guterres/adriano/libraryapi/LibraryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guterres.adriano.libraryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
